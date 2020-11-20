628cfbcd0e2ab25d2fc27ebb735bb21c5d29dda1
AndriyKachkarov <Andriy.Kachkarov@gmail.com>
2020-11-21 00:28:35 +0200

    add display of no-items image

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {StyleSheet, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList, Image} from 'react-native';
 import {AddTodo} from "../components/AddTodo";
 import {Todo} from "../components/Todo";
 
 export const MainScreen = ({addTodo, removeTodo, todos, openTodo}) => {
-  return (
-    <View style={styles.container}>
-        <AddTodo onSubmit={addTodo}/>
-
+    let content = (
         <FlatList
             keyExtractor={item => item.id}
             data={todos}
             renderItem={({item}) => (<Todo todo={item} onRemove={removeTodo} onOpen={openTodo}/>)}
             style={styles.list}
         />
-    </View>
-  )
+    );
+
+    if (todos.length === 0) {
+        content = (
+          <View style={styles.imgWrap}>
+              <Image style={styles.image} source={require('../../assets/no-items.png')}/>
+          </View>
+        );
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <AddTodo onSubmit={addTodo}/>
+            {content}
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
@@ -24,4 +36,15 @@ const styles = StyleSheet.create({
         paddingHorizontal: 30,
         paddingVertical: 20
     },
+    imgWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 50,
+        height: 300
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
 });
